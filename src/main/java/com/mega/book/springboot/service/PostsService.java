b3d81ff99f89ee8b1bd2b1690b90689ddf1c7fbc
kangyoseong <yosung90@gmail.com>
2023-03-20 13:48:45 +0900

    1. 게시판 완성 2. 스피링 시큐리티 의존성 추가 3. 인증관련 구글 세팅 4. api 기능 추가중...

--- src/main/java/com/mega/book/springboot/service/PostsService.java
+++ src/main/java/com/mega/book/springboot/service/PostsService.java
@@ -2,6 +2,7 @@ package com.mega.book.springboot.service;
 
 import com.mega.book.springboot.domain.posts.Posts;
 import com.mega.book.springboot.domain.posts.PostsRepository;
+import com.mega.book.springboot.web.dto.PostsListResponseDto;
 import com.mega.book.springboot.web.dto.PostsResponseDto;
 import com.mega.book.springboot.web.dto.PostsSaveRequestDto;
 import com.mega.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -35,6 +38,21 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->
+                        new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
     }
 
 }
--- src/main/java/com/mega/book/springboot/web/PostsApiController.java
+++ src/main/java/com/mega/book/springboot/web/PostsApiController.java
@@ -5,6 +5,7 @@ import com.mega.book.springboot.web.dto.PostsResponseDto;
 import com.mega.book.springboot.web.dto.PostsSaveRequestDto;
 import com.mega.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -27,4 +28,12 @@ public class PostsApiController {
     public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+
+
 }
